4583f45e7a9fc321d515d6ddbae827ee5f9be1da
yuler <is.yuler@gmail.com>
2022-02-09 11:05:08 +0800

    chore(01-release-and-auto-update): update readme & format code

--- 01-release-and-auto-update/main.js
+++ 01-release-and-auto-update/main.js
@@ -1,33 +1,33 @@
-const path = require("path");
-const { app, BrowserWindow } = require("electron");
-const { autoUpdater } = require("electron-updater");
+const path = require('path')
+const {app, BrowserWindow} = require('electron')
+const {autoUpdater} = require('electron-updater')
 
-const MINUTE = 1000 * 60;
+const MINUTE = 1000 * 60
 setInterval(() => {
-  autoUpdater.checkForUpdatesAndNotify();
-}, MINUTE);
-autoUpdater.checkForUpdatesAndNotify();
+  autoUpdater.checkForUpdatesAndNotify()
+}, MINUTE)
+autoUpdater.checkForUpdatesAndNotify()
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      preload: path.join(__dirname, 'preload.js'),
     },
-  });
+  })
 
-  win.loadFile("index.html");
+  win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-  createWindow();
+  createWindow()
 
-  app.on("activate", function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
-app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") app.quit();
-});
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
